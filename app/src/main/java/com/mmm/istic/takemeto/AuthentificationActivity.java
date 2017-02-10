1b7f78c7a5a1f20c4d50520e893b7d598ad77ec5
yousra <yousraelghzizal@hotmail.fr>
2017-02-10 15:56:43 +0100

    add Activity for authentification

---
+++ app/src/main/java/com/mmm/istic/takemeto/AuthentificationActivity.java
@@ -0,0 +1,13 @@
+package com.mmm.istic.takemeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthentificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentification);
+    }
+}
