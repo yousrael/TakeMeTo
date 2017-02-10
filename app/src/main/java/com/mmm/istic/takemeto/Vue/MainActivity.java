6ef7b24527a53cdd36f7916559104c680944bfac
sp.M2GLa <Na@na.na>
2017-02-10 15:44:29 +0100

    structure package

--- app/src/main/java/com/mmm/istic/takemeto/Vue/MainActivity.java
+++ app/src/main/java/com/mmm/istic/takemeto/Vue/MainActivity.java
@@ -1,7 +1,9 @@
-package com.mmm.istic.takemeto;
+package com.mmm.istic.takemeto.Vue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mmm.istic.takemeto.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/mmm/istic/takemeto/model/User.java
@@ -0,0 +1,8 @@
+package com.mmm.istic.takemeto.model;
+
+/**
+ * Created by stephen on 10/02/17.
+ */
+
+public class User {
+}
