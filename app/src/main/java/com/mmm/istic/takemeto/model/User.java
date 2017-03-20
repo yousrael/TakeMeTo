d1506f265fdc657eb10edae9919cd3b91f23be56
bs <bizimungu.steve@gmail.com>
2017-03-20 14:33:02 +0100

    user getter setter

--- app/src/main/java/com/mmm/istic/takemeto/model/User.java
+++ app/src/main/java/com/mmm/istic/takemeto/model/User.java
@@ -12,6 +12,39 @@ public class User {
     private String prenom;
     private String mail ;
     private String phone ;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     private ArrayList<Trajet> trajetsPropose;
     private ArrayList<Trajet> reservations ;
 
