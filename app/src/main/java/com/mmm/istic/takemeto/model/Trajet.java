325051441d0356d02e52251bc9008378e7a65f76
bs <bizimungu.steve@gmail.com>
2017-03-20 16:17:32 +0100

    trajet updated

--- app/src/main/java/com/mmm/istic/takemeto/model/Trajet.java
+++ app/src/main/java/com/mmm/istic/takemeto/model/Trajet.java
@@ -4,56 +4,93 @@ package com.mmm.istic.takemeto.model;
  * Created by steve on 18/03/17.
  */
 
+import android.location.Address;
+
 import java.sql.Date;
+
+import java.util.List;
 
 /**
 Classe representant un trajet de l'application
  */
 public class Trajet {
 
-    private User user ;
-    private Date dateDepart ;
-    private String lieuDepart;
-    private String lieuArrivee;
+    private String user ;
+    private java.util.Date dateDepart ;
+    private Address addressDepart;
+    private Address addressArrivee;
     private int prixTrajet;
+    private List<String> vayageurs ;
 
-    public Trajet(User user, Date dateDepart, String lieuDepart, String lieuArrivee, int prixTrajet) {
+    public Trajet(String user, java.util.Date dateDepart, Address addressdepart, Address addressarrive, int prixTrajet, int places) {
         this.user = user;
         this.dateDepart = dateDepart;
-        this.lieuDepart = lieuDepart;
-        this.lieuArrivee = lieuArrivee;
+        this.addressDepart = addressdepart;
+        this.addressArrivee = addressarrive;
         this.prixTrajet = prixTrajet;
+        this.places = places;
+
     }
-    public User getUser() {
+
+
+
+    public int getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(int places) {
+        this.places = places;
+    }
+
+    public void setDateDepart(java.util.Date dateDepart) {
+        this.dateDepart = dateDepart;
+    }
+
+    private int places ;
+
+    public List<String> getVayageurs() {
+        return vayageurs;
+    }
+
+    public void setVayageurs(List<String> vayageurs) {
+        this.vayageurs = vayageurs;
+    }
+
+
+
+    public Address getAddressdepart() {
+        return addressDepart;
+    }
+
+    public void setAddressdepart(Address addressdepart) {
+        this.addressDepart = addressdepart;
+    }
+
+    public Address getAddressarrive() {
+        return addressArrivee;
+    }
+
+    public void setAddressarrive(Address addressarrive) {
+        this.addressArrivee = addressarrive;
+    }
+
+
+
+
+    public String getUser() {
         return user;
     }
 
-    public void setUser(User user) {
+    public void setUser(String user) {
         this.user = user;
     }
 
-    public Date getDateDepart() {
+    public java.util.Date getDateDepart() {
         return dateDepart;
     }
 
     public void setDateDepart(Date dateDepart) {
         this.dateDepart = dateDepart;
-    }
-
-    public String getLieuDepart() {
-        return lieuDepart;
-    }
-
-    public void setLieuDepart(String lieuDepart) {
-        this.lieuDepart = lieuDepart;
-    }
-
-    public String getLieuArrivee() {
-        return lieuArrivee;
-    }
-
-    public void setLieuArrivee(String lieuArrivee) {
-        this.lieuArrivee = lieuArrivee;
     }
 
     public int getPrixTrajet() {
@@ -64,9 +101,34 @@ public class Trajet {
         this.prixTrajet = prixTrajet;
     }
 
+    /**
+     * Ajoute un vayageur au trajet
+     * @param user l'identifinat de l'utilisateur
+     * @return true si l'utilisateur a été ajouté faux si non
+     */
+    public boolean addVoyageur (String user){
+        boolean res = nombresPlacesDisponible()>0;
+        if(res){
+            this.vayageurs.add(user);
+        }
+       return res;
+
+    }
+
+    /**
+     * Retourne le nombre de places disponible sur le trajet
+     * @return un nombre positif de places disponibles 0 si il ne reste pas de place
+     */
+    public int nombresPlacesDisponible(){
+        if (this.vayageurs.size() < this.places){
+            return this.vayageurs.size()- this.places ;
+        }
+        else {
+            return 0;
+        }
 
 
-
+    }
 
 
 }
