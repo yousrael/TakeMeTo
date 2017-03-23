716e7d05d60528277e20701e6b9e37e4bc12fbea
yousra <yousraelghzizal@hotmail.fr>
2017-03-23 14:30:45 +0100

    Get the Current User

--- app/src/main/java/com/mmm/istic/takemeto/Vue/HomeActivity.java
+++ app/src/main/java/com/mmm/istic/takemeto/Vue/HomeActivity.java
@@ -1,14 +1,19 @@
 package com.mmm.istic.takemeto.Vue;
 
 import android.content.Intent;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.mmm.istic.takemeto.R;
 
 public class HomeActivity extends AppCompatActivity {
@@ -70,14 +75,62 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void affichProfil() {
-        Intent i=new Intent(HomeActivity.this,ProfilActivity.class);
-        startActivity(i);
-    }
+      //  FirebaseAuth.AuthStateListener mAuthListener;
+      final  Intent i;
+
+         i=new Intent(HomeActivity.this,ProfilActivity.class);
+       // mAuthListener = new FirebaseAuth.AuthStateListener() {
+           /* @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {*/
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    i.putExtra("requestCode",0);
+                    i.putExtra("email", user.getEmail());
+                    Log.d("", "onAuthStateChanged:signed_in:" + user.getUid());
+                    startActivity(i);
+
+                } else {
+                    // User is signed out
+                    Log.d("", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+
+        // ...
+
 
     private void getTrips() {
         Intent i=new Intent(HomeActivity.this,MyListTripsActivity.class);
         startActivity(i);
 
     }
+   /* mAuthListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                // User is signed in
+                Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+            } else {
+                // User is signed out
+                Log.d(TAG, "onAuthStateChanged:signed_out");
+            }
+            // ...
+        }
+    };
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }*/
 
 }
--- app/src/main/java/com/mmm/istic/takemeto/Vue/ProfilActivity.java
+++ app/src/main/java/com/mmm/istic/takemeto/Vue/ProfilActivity.java
@@ -3,14 +3,24 @@ package com.mmm.istic.takemeto.Vue;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.mmm.istic.takemeto.R;
+import com.mmm.istic.takemeto.model.User;
 
 public class ProfilActivity extends AppCompatActivity {
 
-    Intent intent = getIntent();
 
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +28,34 @@ public class ProfilActivity extends AppCompatActivity {
 
         Button button = (Button)findViewById(R.id.button1);
 
-        if(intent.getIntExtra("requestCode",0) == 0){
+        Intent intent = getIntent();
+        int requestCode = intent.getIntExtra("requestCode",1);
+        if(requestCode == 0){
             //Own user account
+           String email= intent.getStringExtra("email");
+        Log.e("get User by email", "email: "+email);
+            databaseReference = FirebaseDatabase.getInstance().getReference("users");
+            Query query= databaseReference.orderByChild("email").equalTo(email);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                       User user= (User) postSnapshot.getValue();
+                        Log.d("get User by email","SUCCESS"+user.getPrenom());
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.d("get User by email","Failure");
+                }
+            });
+
+
+
             button.setEnabled(true);
         }
-        else if(intent.getIntExtra("requestCode",1) == 1){
+        else if(requestCode == 1){
             //Other user account
             button.setText("Send message");
             button.setEnabled(false);
