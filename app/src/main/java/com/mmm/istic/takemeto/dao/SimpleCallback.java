728e64ad86b655a138ac543bcca0acc690f71040
sp.M2GLa <Na@na.na>
2017-03-25 13:27:48 +0100

    add UserDao

---
+++ app/src/main/java/com/mmm/istic/takemeto/dao/SimpleCallback.java
@@ -0,0 +1,10 @@
+package com.mmm.istic.takemeto.dao;
+
+/**
+ * Created by stephen on 25/03/17.
+ * http://stackoverflow.com/questions/42128909/return-value-from-valueeventlistener-java
+ */
+
+public interface SimpleCallback<T> {
+    void callback(T data);
+}
